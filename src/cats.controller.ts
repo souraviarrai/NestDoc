1d81788125cddcf437fc20c15daea25cff1416a8
Sourav Rai <souraviarrai@gmail.com>
2023-12-12 14:03:39 +0600

    done with dynamic parameters

--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get, Post, HttpCode, Header, Redirect} from '@nestjs/common';
+import {Controller, Get, Post, HttpCode, Header, Redirect, Param} from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
@@ -8,6 +8,11 @@ export class CatsController {
     @Redirect('https://souravrai.netlify.app/', 301)
     findAll(): string {
         return 'This is sourav the killer';
+    }
+    @Get(':id')
+    findOne(@Param() params: any): string {
+        console.log(params.id);
+        return `This returns the cat with id ${params.id}`;
     }
     @Post()
     @HttpCode(204)
